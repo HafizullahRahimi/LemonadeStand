8771d791ac07e1047f7ff475d7f71fe9f533ca26
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2024-01-04 15:45:24 +0100

    Add LemonadeStand.Presentation

--- LemonadeStand.Presentation/Program.cs
+++ LemonadeStand.Presentation/Program.cs
@@ -1,16 +1,13 @@
-using LemonadeStand.BlazorApp;
+using LemonadeStand.Presentation;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LemonadeStand.Application;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.ConfigureApplicationServices();
 
 await builder.Build().RunAsync();
---
+++ LemonadeStand.Presentation/Validator/OrderDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using LemonadeStand.Application.DTOs;
+
+namespace LemonadeStand.Presentation.Validator
+{
+    public class OrderDtoValidator : AbstractValidator<OrderDto>
+    {
+        public OrderDtoValidator()
+        {
+            RuleFor(o => o.Recipe).IsInEnum().WithMessage("Choose a recipe");
+            RuleFor(o => o.OrderedQuanity).NotNull().WithMessage("Ordered Quanity must not be empty").GreaterThan(0).WithMessage("Select the quantity ordered");
+            RuleFor(o => o.MoneyPaid).NotNull().WithMessage("Money Paid must not be empty").GreaterThan(0).WithMessage("Money paid only enough for 0 glass(es)!");
+        }
+    }
+}
